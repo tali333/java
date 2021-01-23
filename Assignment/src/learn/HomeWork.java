48888da589c16504a09525f872c7f295d41d6ce5
tali <talimarks11@gmail.com>
2021-01-23 17:41:08 -0600

    gg

---
+++ Assignment/src/learn/HomeWork.java
@@ -0,0 +1,14 @@
+package learn;
+
+import java.util.Scanner;
+
+public class HomeWork {
+	public static void main(String[] args) {
+	 
+		Scanner ui = new Scanner (System.in);
+	
+		System.out.println("what are you doing :");
+	    String data = ui.next();
+	}	
+
+}
